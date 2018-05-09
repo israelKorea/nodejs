9bda78bbb3590a3ba3efb51e9974bfaf0e880376
israelKorea <jhys0226@naver.com>
2018-05-10 01:27:53 +0900

    web app add jade and view

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var app = express();
 
+app.locals.pretty = true;
+app.use(express.static('public'));
+app.set('view engine', 'jade');
+app.set('views', './views');
+
 app.get('/', function(req, res){
   res.send('Hello World! ~!!');
 });
 app.get('/route', function(req, res){
   res.send('hi, Route <img src="/aws.png">');
 });
-
+app.get('/template', function(req, res){
+  res.render('temp', {time: Date(), _title:'JadeTemplate'});
+});
 app.get('/login', function(req, res){
   res.send('<h1>login please</h1>');
 });
-
-app.use(express.static('public'));
 app.listen(3000, function(){
   console.log('example app listening on port 3000!');
 });
