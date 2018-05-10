c51b4b145176c560723720430323b8de79b09eb5
israelKorea <jhys0226@naver.com>
2018-05-10 15:12:35 +0900

    commit node web

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ var express = require('express');
 var app = express();
 
 app.locals.pretty = true;
-app.use(express.static('public'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.set('views', './views');
-
+app.use(express.static('public'));
 app.get('/', function(req, res){
   res.send('Hello World! ~!!');
 });
@@ -36,6 +35,10 @@ for(var i=0; i<5; i++){
 app.get('/template', function(req, res){
   res.render('temp', {time: Date(), _title:'JadeTemplate'});
 });
+app.get('/haeyoon', function(req,res){
+  res.render('haeyoon', {_nm:'Johaeyoon'});
+});
+
 app.get('/login', function(req, res){
   res.send('<h1>login please</h1>');
 });
