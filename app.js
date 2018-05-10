16e2a9456094ddbd30e0b52e2d0b31963160b8d4
israelKorea <jhys0226@naver.com>
2018-05-10 12:28:01 +0900

    commit app.js

--- app.js
+++ app.js
@@ -8,6 +8,29 @@ app.get('/route', function(req, res){
   res.send('hi, Route <img src="/aws.png">');
 });
 
+app.get('/dynamic', function(req, res){
+
+var lis = '';
+for(var i=0; i<5; i++){
+  lis = lis + '<li>coding</li>';
+}
+
+  var output = `
+<!DOCTYPE html>
+<html>
+ <head>
+    <meta charset="utf-8">
+ </head>
+ <body>
+    Welcome to my first dynamic webapp
+    <ul>
+      ${lis}
+    </ul>
+ </body>
+</html>
+`;
+  res.send(output);
+});
 app.get('/login', function(req, res){
   res.send('<h1>login please</h1>');
 });
