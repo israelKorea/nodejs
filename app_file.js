2bb04acbd4a23a6b3dbf6c3ed26ec869738403cc
israelKorea <jhys0226@naver.com>
2018-05-27 04:38:06 +0900

    web build

---
+++ app_file.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.locals.pretty = true;
+app.set('views', 'views_file');
+app.engine('pug', require('pug').__express);
+app.set('view engine', 'pug');
+
+app.get('/topic/new', function(req, res){
+	res.render('new');	
+});
+app.get('/topic', function(req, res){
+	fs.readdir('data', function(err, files){
+		if(err){
+			console.log(err);
+			res.status(500).send('Internal Server Error');
+		}
+		res.render('view', {topics : files});
+	});
+	
+});
+
+app.get('/topic/:id', function(req, res){
+	var id = req.params.id;
+	
+	fs.readdir('data', function(err, files){
+		if(err){
+			console.log(err);
+			res.status(500).send('Internal Server Error');
+		}
+		
+		fs.readFile('data/'+ id, 'utf8', function(err, data){
+		if(err){
+			console.log(err);
+			res.status(500).send('Internal Server Error');
+		}
+		res.render('view', {topics:files, title:id, desc: data});
+		});
+	});
+	
+	
+});
+
+app.post('/topic', function(req, res){
+	var title = req.body.title;
+	var description = req.body.description;
+	fs.writeFile('data/'+ title,description, function(err){
+		if(err){
+			console.log(err);
+			res.status(500).send('Internal Server Error');
+		}
+		res.send('Success!');
+	});	
+});
+
+app.listen(3000, function(){
+	console.log('Server running on 3000 port!');
+});
--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@
 start Node.js
 
 Create basic web server based Node
+
+2018.05.26
+we create web app as save data to files
